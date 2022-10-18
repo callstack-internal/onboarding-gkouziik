2f22d2b71145e4f8d2be365c92a19268cda0960f
gkouziik <gkouziik@gmail.com>
2022-10-18 15:24:31 +0300

    Added Navigation dependencies and Created a dummy Stack Navigator flow

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { MainNavigator } from './src/navigators';
 
-const App = () => {
+const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   };
 
   return (
@@ -16,6 +19,9 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
     </SafeAreaView>
   );
 };
